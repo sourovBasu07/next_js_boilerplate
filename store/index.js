43a69b11bff13210637e268c4e8ada808e4e9f15
souravbasu07 <sourov_basu@coredevs.ltd>
2024-07-10 17:35:01 +0600

    rtk query added

--- store/index.js
+++ store/index.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "./reducers/counterSlice";
+import { proudctApi } from '@/services/api/apiSlice';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        [proudctApi.reducerPath]: proudctApi.reducer
     },
-})
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(proudctApi.middleware)
+});
+
+setupListeners(store.dispatch);
